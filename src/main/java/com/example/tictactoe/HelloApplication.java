6b0199b9d239759ca63a09b6d112cad2566a8f56
RomanSavitski <roma.savitskiy@inbox.ru>
2021-12-01 19:17:18 +0300

    Initial commit

---
+++ src/main/java/com/example/tictactoe/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.example.tictactoe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Main window
+ */
+public class HelloApplication extends Application {
+
+    /**
+     *
+     * @param stage
+     * @throws IOException
+     */
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainMenu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
+        stage.setTitle("App!");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+//проверка
+}
